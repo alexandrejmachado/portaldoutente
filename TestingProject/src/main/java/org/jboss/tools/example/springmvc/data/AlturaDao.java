2a46a3806504b7ed039b121a2fa1ae30927b5e81
Tiago Mauricio <fc45104@alunos.fc.ul.pt>
2016-05-09 16:45:47 +0100

    comeco dos DAOS

--- TestingProject/src/main/java/org/jboss/tools/example/springmvc/data/AlturaDao.java
+++ TestingProject/src/main/java/org/jboss/tools/example/springmvc/data/AlturaDao.java
@@ -1,8 +1,11 @@
 package org.jboss.tools.example.springmvc.data;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 import org.jboss.tools.example.springmvc.model.Altura;
+import org.jboss.tools.example.springmvc.sensitivedata.Utente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,6 +21,13 @@ public class AlturaDao {
 		Altura altura = new Altura(valor, numUtente);
 		em.persist(altura);
 		return altura;
+	}
+	
+	public List<Altura> findAllByUtente(int numUtente){
+		//TODO ver a cena de crifrar
+		TypedQuery<Altura> query = em.createNamedQuery(Altura.FIND_ALL_BY_UTENTE, Altura.class);
+		query.setParameter(Altura.UTENTE, numUtente);
+		return null;
 	}
 	
 }
