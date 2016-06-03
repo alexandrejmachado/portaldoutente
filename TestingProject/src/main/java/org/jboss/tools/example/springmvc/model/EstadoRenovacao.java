954afea8660069b1ddfe567e2eca1ed209dd8a0a
Pedro <pneves6@hotmail.com>
2016-06-03 15:58:18 +0100

    medicacao halfway

---
+++ TestingProject/src/main/java/org/jboss/tools/example/springmvc/model/EstadoRenovacao.java
@@ -0,0 +1,7 @@
+package org.jboss.tools.example.springmvc.model;
+
+public enum EstadoRenovacao {
+
+	PENDENTE,ACEITE,CADUCADO,REJEITADO
+	
+}
--- TestingProject/src/main/java/org/jboss/tools/example/springmvc/model/Medicacao.java
+++ TestingProject/src/main/java/org/jboss/tools/example/springmvc/model/Medicacao.java
@@ -1,5 +1,6 @@
 package org.jboss.tools.example.springmvc.model;
 
+import java.util.Calendar;
 import java.util.Date;
 
 import javax.persistence.Entity;
@@ -13,7 +14,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @NamedQueries({
-	@NamedQuery(name=Medicacao.FIND_ALL_BY_UTENTE, query="SELECT m FROM Medicacao m WHERE m.numUtente = :" + Medicacao.UTENTE +" ORDER BY m.data DESC")
+	@NamedQuery(name=Medicacao.FIND_ALL_BY_UTENTE, query="SELECT m FROM Medicacao m WHERE m.numUtente = :" + Medicacao.UTENTE +" ORDER BY m.cal DESC")
 })
 public class Medicacao {
 
@@ -27,17 +28,124 @@ public class Medicacao {
 	public static final String UTENTE = "numUtente";
 	
 	@JsonIgnore
-	private int numUtente;
+	private String numUtente;
 	
 	@Temporal(TemporalType.TIMESTAMP)
-	private Date data;
+	private Calendar cal;
 	
 	@JsonIgnore
 	private int idMedicamento;
 	
-	private int dose;
+	private boolean estado = false; //esta a true se o medico ja confirmou que esta medicação existe, caso nao tenha confirmado manter-se-a a falso
+
+	private EstadoRenovacao renovacao;
+	
+	private double dose;
 	
 	private String indicacoes;
 	
+	private int comprimidosPorCaixa;
+	
 	public Medicacao(){}
+	
+	public Medicacao(String numUtente, int idMedicamento, double dose, String indicacoes, String renovacao, int comprimidosPorCaixa) {
+		this.numUtente = numUtente;
+		this.idMedicamento = idMedicamento;
+		this.dose = dose;
+		this.indicacoes = indicacoes;
+		setRenovacao(renovacao);
+		this.comprimidosPorCaixa = comprimidosPorCaixa;
+		
+		cal = Calendar.getInstance();
+		cal.add(Calendar.DATE, getDiasParaRenovacao() - 5);
+	}
+	
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNumUtente() {
+		return numUtente;
+	}
+
+	public void setNumUtente(String numUtente) {
+		this.numUtente = numUtente;
+	}
+
+	public Date getDataFinal() {
+		return cal.getTime();
+	}
+
+	public void setCal(Calendar cal) {
+		this.cal = cal;
+	}
+	
+	public String getRenovacao() {
+		switch (renovacao) {
+		case ACEITE:
+			return "Aceite";
+		case PENDENTE:
+			return "Pendente";
+		case CADUCADO:
+			return "Caducado";
+		default:
+			return "Rejeitado";
+		}
+	}
+	
+	public void setRenovacao(String renovacao) {
+		switch (renovacao) {
+		case "Aceite":
+			this.renovacao = EstadoRenovacao.ACEITE;
+		case "Pendente":
+			this.renovacao = EstadoRenovacao.PENDENTE;
+		case "Caducado":
+			this.renovacao = EstadoRenovacao.CADUCADO;
+		default:
+			this.renovacao = EstadoRenovacao.REJEITADO;
+	}
+	}
+
+	public int getIdMedicamento() {
+		return idMedicamento;
+	}
+
+	public void setIdMedicamento(int idMedicamento) {
+		this.idMedicamento = idMedicamento;
+	}
+
+	public double getDose() {
+		return dose;
+	}
+
+	public void setDose(double dose) {
+		this.dose = dose;
+	}
+	
+	public boolean getEstado() {
+		return estado;
+	}
+	
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
+
+	public String getIndicacoes() {
+		return indicacoes;
+	}
+
+	public void setIndicacoes(String indicacoes) {
+		this.indicacoes = indicacoes;
+	}
+
+	public int getDiasParaRenovacao() {
+		return (int) (comprimidosPorCaixa/dose);
+	}
+	
+	
 }
